53526922ebfd9d8f30848efb4d65899aecbdf978
Angus Kellett <anguskellett@hotmail.com>
2020-11-16 23:28:03 +0000

    removes setupInput and handleUserInput; requires setupInput from input.js

--- play.js
+++ play.js
@@ -1,28 +1,7 @@
 const { connect } = require('./client');
-
-/**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  // register handleUserInput here
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
-
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
+
 setupInput();
