aaf4cb4cd31d29ec1f06c0c3d01ce5c11ae11c8c
Angus Kellett <anguskellett@hotmail.com>
2020-11-16 23:37:02 +0000

    adds WASD keys to handleUserInput

--- input.js
+++ input.js
@@ -14,6 +14,18 @@ const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
   }
+  if (data === '\u0077') {
+    console.log("up");
+  }
+  if (data === '\u0073') {
+    console.log("down");
+  }
+  if (data === '\u0064') {
+    console.log("right");
+  }
+  if (data === '\u0061') {
+    console.log("left");
+  }
 };
 
 module.exports = { setupInput };
