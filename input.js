71235b780df990f5795eebc673919ace1b97076d
Angus Kellett <anguskellett@hotmail.com>
2020-11-17 01:19:01 +0000

    adds connection variable; writes directions to server (without success)

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -15,17 +18,17 @@ const handleUserInput = function(data) {
     process.exit();
   }
   if (data === '\u0077') {
-    console.log("up");
-  }
+    connection.write('Move: up');
+  };
   if (data === '\u0073') {
-    console.log("down");
-  }
+    connection.write('Move: down');
+  };
   if (data === '\u0064') {
-    console.log("right");
-  }
+    connection.write('Move: right');
+  };
   if (data === '\u0061') {
-    console.log("left");
-  }
+    connection.write('Move: left');
+  };
 };
 
 module.exports = { setupInput };
